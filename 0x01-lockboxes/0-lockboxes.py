2150cb823c4d2ae376b1a38b973d1577e13e9206
DaBetterman <1ebrahimr@gmail.com>
2024-07-03 21:23:50 +0200

    changed code slightly

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
+"""
+Solution to lockbox problem
+"""
+
 
 def canUnlockAll(boxes):
     """
     Determine if all boxes can be opened.
     """
-    n = len(boxes)
-    opened_boxes = set([0])
-    keys = set(boxes[0])
+    if not isinstance(boxes, list) or len(boxes) == 0:
+        return False
+
+    # A set to keep track of opened boxes
+    opened = {0}
+    # A list to keep track of keys to be processed
+    keys = [0]
 
     while keys:
-        key = keys.pop()
-        if key < n and key not in opened_boxes:
-            opened_boxes.add(key)
-            keys.update(boxes[key])
+        current = keys.pop()
+        for key in boxes[current]:
+            if key not in opened:
+                opened.add(key)
+                if key < len(boxes):
+                    keys.append(key)
 
-    return len(opened_boxes) == n
+    return len(opened) == len(boxes)

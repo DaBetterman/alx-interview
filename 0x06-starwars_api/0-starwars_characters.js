fc06c80a1925b9db906d29291a38914765aa5903
DaBetterman <1ebrahimr@gmail.com>
2024-08-08 21:08:14 +0200

    star wars

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  
+  const characters = JSON.parse(body).characters;
+  characters.forEach((character) => {
+    request(character, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+      console.log(JSON.parse(body).name);
+    });
+  });
+});
